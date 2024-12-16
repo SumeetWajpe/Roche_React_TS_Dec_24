00c250ca4e6814c8d7fb51d3ad019deb750e093f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-16 12:26:44 +0530

    ✨ Spread Operator

--- DAY1/Typescript/Basics.js
+++ DAY1/Typescript/Basics.js
@@ -1,22 +1,48 @@
 // var str = "Hello Typescript !"; // Type Inference
-var str; // Type annotation
-str = "Hello TS !";
+// var str: string; // Type annotation
+// str = "Hello TS !";
 // str = 10;
-console.log(str);
-var b;
-var n;
-var a;
-var obj = { name: "Roche" };
-var arr = ["React", "Redux", "Angular"];
-// Generics
-var companies = new Array();
+// console.log(str);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// var b: boolean;
+// var n: number;
+// var a: any;
+// var obj = { name: "Roche" };
+// var arr: string[] = ["React", "Redux", "Angular"];
+// // Generics
+// var companies: string[] = new Array<string>();
 // companies[0] = 100;
 // Creating variables
 // let -> ES6
-if (true) {
-    var x_1 = 1000; // block scoping
-    // after 10 lines of code
-    console.log(x_1);
-}
-var x = 100;
-console.log(x);
+// var x: number = 10;
+// if (true) {
+//   let x: number = 1000; // block scoping
+//   // after 10 lines of code
+//   //   let x: number = 100000;
+//   console.log(x);
+// }
+// let s = "Hello !";
+// const y: number = 30; // block scoped
+// y = 20;
+// const player = { name: "Alcaraz" };
+// // player.xyz = "XYZ";
+// player.name = "Carlos Alcaraz";
+// console.log(player.name);
+// Operators
+// - Spread Operator - Object & Array
+// let person = { name: "Alcaraz", age: 20 };
+// let player = { ...person, sport: "Tennis", isWimbledonChampion: true };
+// console.log(player);
+/// with Arrays
+var techs = ["React", "Redux", "Angular"];
+var otherTechs = ["C#", "Kubenetes", "GraphQL"];
+var allTechs = __spreadArray(__spreadArray(__spreadArray([], techs, true), otherTechs, true), ["HTML5"], false);
+console.log(allTechs);
--- DAY1/Typescript/Basics.ts
+++ DAY1/Typescript/Basics.ts
@@ -1,27 +1,49 @@
 // var str = "Hello Typescript !"; // Type Inference
-var str: string; // Type annotation
-str = "Hello TS !";
+// var str: string; // Type annotation
+// str = "Hello TS !";
 // str = 10;
-console.log(str);
+// console.log(str);
 
-var b: boolean;
-var n: number;
-var a: any;
-var obj = { name: "Roche" };
-var arr: string[] = ["React", "Redux", "Angular"];
-// Generics
-var companies: string[] = new Array<string>();
+// var b: boolean;
+// var n: number;
+// var a: any;
+// var obj = { name: "Roche" };
+// var arr: string[] = ["React", "Redux", "Angular"];
+// // Generics
+// var companies: string[] = new Array<string>();
 // companies[0] = 100;
 
 // Creating variables
 
 // let -> ES6
+// var x: number = 10;
+// if (true) {
+//   let x: number = 1000; // block scoping
+//   // after 10 lines of code
+//   //   let x: number = 100000;
+//   console.log(x);
+// }
 
-if (true) {
-  let x: number = 1000; // block scoping
-  // after 10 lines of code
-  console.log(x);
-}
-let x: number = 100;
+// let s = "Hello !";
+// const y: number = 30; // block scoped
+// y = 20;
+// const player = { name: "Alcaraz" };
+// // player.xyz = "XYZ";
+// player.name = "Carlos Alcaraz";
+// console.log(player.name);
 
-console.log(x);
+// Operators
+// - Spread Operator - Object & Array
+
+// let person = { name: "Alcaraz", age: 20 };
+
+// let player = { ...person, sport: "Tennis", isWimbledonChampion: true };
+
+// console.log(player);
+
+/// with Arrays
+
+var techs: string[] = ["React", "Redux", "Angular"];
+let otherTechs: string[] = ["C#", "Kubenetes", "GraphQL"];
+let allTechs: string[] = [...techs, ...otherTechs, "HTML5"];
+console.log(allTechs);
