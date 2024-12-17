0680c367b605e68d092ad7cd33c73ef0858f7fe1
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-17 12:25:25 +0530

    ✨ Working with props as object

--- DAY2/my-react-app/src/components/app/App.jsx
+++ DAY2/my-react-app/src/components/app/App.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Message from "../message/message";
-import "./App.css";
 
 class App extends React.Component {
+  messageOne = {
+    msg: "Hello",
+    from: "John",
+    to: "Carter",
+    imageUrl:
+      "https://as2.ftcdn.net/v2/jpg/05/13/79/77/1000_F_513797754_KyKftzXhlul8FalDksafJf1TmtqrX1Px.jpg",
+  };
   render() {
     return (
       <>
-        <Message msg="Hello" from="John" to="Carter" />
-        <Message msg="Hey" from="James" to="Bond" />
-        <Message msg="Hi" from="Peter" to="Parker" />
+        <Message msgdetails={this.messageOne} />
+        {/* <Message
+          msg="Hey"
+          from="James"
+          to="Bond"
+          imageUrl="https://c8.alamy.com/comp/2RB5J3G/hey-text-speech-bubble-hey-hi-hello-psst-hey-word-on-text-box-vector-illustration-hand-drawn-quote-hey-icon-lettering-doodle-phrase-design-f-2RB5J3G.jpg"
+        />
+        <Message
+          msg="Hi"
+          from="Peter"
+          to="Parker"
+          imageUrl="https://c8.alamy.com/comp/2CA3NX4/hi-word-in-splashs-background-2CA3NX4.jpg"
+        /> */}
       </>
     );
   }
--- DAY2/my-react-app/src/components/message/message.jsx
+++ DAY2/my-react-app/src/components/message/message.jsx
@@ -4,9 +4,15 @@ class Message extends React.Component {
   render() {
     return (
       <>
-        <h2>{this.props.msg}</h2>
-        <p>From : {this.props.from}</p>
-        <p>To : {this.props.to}</p>
+        <h2>{this.props.msgdetails.msg}</h2>
+        <img
+          src={this.props.msgdetails.imageUrl}
+          alt={this.props.msgdetails.msg}
+          height="100px"
+          width="150px"
+        />
+        <p>From : {this.props.msgdetails.from}</p>
+        <p>To : {this.props.msgdetails.to}</p>
       </>
     );
   }
