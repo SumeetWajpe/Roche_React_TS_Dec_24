4a81b8a046b1a9c355a33fe775d3503dab1d2479
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-18 14:19:13 +0530

    ✨ state as object

--- DAY2/Rochedemy/src/components/counter/counter.tsx
+++ DAY2/Rochedemy/src/components/counter/counter.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 const Counter: React.FC = () => {
-  let [count, setCount] = useState(100);
+  let [data, setData] = useState({ count: 100, age: 18 });
+  //   let [count, setCount] = useState(100);
+  //   let [age, setAge] = useState(18);
+
   return (
     <div>
-      <p> Count : {count} </p>
-      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+      <p> Count : {data.count} </p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setData({ ...data, count: data.count + 1 })}
+      >
         {" "}
-        ++{" "}
+        Count++{" "}
       </button>
+      <hr />
+      <p> Age : {data.age} </p>
+      <button className="btn btn-primary"> Age++ </button>
     </div>
   );
 };
