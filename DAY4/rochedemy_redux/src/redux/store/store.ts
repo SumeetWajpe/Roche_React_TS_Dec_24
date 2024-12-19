081861505307ec9e8dd60283aa892245b1cd4b8d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-19 16:41:52 +0530

    ✨ using react-redux | Provider & useSelector()

---
+++ DAY4/rochedemy_redux/src/redux/store/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import courses from "../reducers/courses.reducer";
+import posts from "../reducers/posts.reducer";
+
+const store = configureStore({
+  reducer: { courses, posts },
+});
+export default store;
