50cf30aea0c2f27ef5b451a858ffe2282dac00e1
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-19 10:49:30 +0530

    ✨ Handling 404

--- DAY2/Rochedemy/src/components/app/App.tsx
+++ DAY2/Rochedemy/src/components/app/App.tsx
@@ -15,6 +15,15 @@ const App: React.FC = () => {
         <Route path="/" Component={ListOfCourses}></Route>
         <Route path="/posts" Component={Posts}></Route>
         <Route path="/getpostbyid" Component={GetPostById}></Route>
+        <Route
+          path="*"
+          element={
+            <>
+              <h1>Page Not Found !</h1>
+              <img src="https://internetdevels.com/sites/default/files/public/blog_preview/404_page_cover.jpg" />
+            </>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
