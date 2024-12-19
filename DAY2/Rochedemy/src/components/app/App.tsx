81c562b35feaa7dd1f49795a32872e3ac282e4e0
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-19 10:11:19 +0530

    ✨ Routing Basics

--- DAY2/Rochedemy/src/components/app/App.tsx
+++ DAY2/Rochedemy/src/components/app/App.tsx
@@ -4,12 +4,18 @@ import ListOfCourses from "../listofcourses/listofcourses";
 import Counter from "../counter/counter";
 import Posts from "../posts/posts";
 import GetPostById from "../getpostbyid/getpostbyid";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
-  return <ListOfCourses />;
-  // return <Counter />;
-  // return <Posts />;
-  // return <GetPostById />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={ListOfCourses}></Route>
+        <Route path="/posts" Component={Posts}></Route>
+        <Route path="/getpostbyid" Component={GetPostById}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
